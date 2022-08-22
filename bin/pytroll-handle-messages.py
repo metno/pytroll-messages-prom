48ca78b1be5acecf7b542f19f92c48347bfbd399
Trygve Aspenes <trygveas@met.no>
2022-08-22 12:35:14 +0200

    cleanup

--- bin/pytroll-handle-messages.py
+++ bin/pytroll-handle-messages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016,2019 Trygve Aspenes
+# Copyright (c) 2022 Trygve Aspenes
 
 # Author(s): Trygve Aspenes
 
@@ -45,9 +45,6 @@ import os
 import json
 import posttroll.message
 
-import mysql.connector
-from mysql.connector import errorcode
-
 
 class MessageHandler(object):
 
@@ -69,13 +66,7 @@ class MessageHandler(object):
         except (NoOptionError, ValueError):
             nameserver = 'localhost'
 
-        try:
-            self.providing_server = config.get(section, 'providing-server').split(" ")
-        except Exception:
-            self.providing_server = None
-
         self._listener = ListenerContainer(topics=topics, nameserver=nameserver)
-        # self._parser = Parser(self._pattern)
 
     def set_logger(self, logger):
         """Set logger."""
@@ -98,7 +89,6 @@ class MessageHandler(object):
             except queue.Empty:
                 continue
 
-            #if msg.type == "file":
             try:
                 msg_host = msg.host.split(".")[0]
             except Exception:
@@ -126,10 +116,7 @@ class MessageHandler(object):
     def process(self, msg):
         """Process message"""
         try:
-            #self.logger.debug("Message: {}".format(msg))
-            meta = dict()
-            write_to_queue(msg, meta, self._queue)
-            #mda = self._parser.parse(msg.data["uid"])
+            write_to_queue(msg, self._queue)
         except ValueError:
             self.logger.debug("Unknown file, skipping.")
             return
@@ -141,24 +128,12 @@ def reset_skip_hosts(skip_hosts):
     except AttributeError:
         skip_hosts.pop()
 
-def read_from_queue(queue, logger, hosts, data_points_before_write):
+def read_from_queue(queue, logger):
     # read from queue
-    orig_hosts = list(hosts)
-    skip_hosts = []
-    last_push = {}
-    message_data = {}
-    statement = "insert into messages (topic, datetime, msg_host, type, jdoc) values (%s, %s, %s, %s, %s)"
-    # set empty list for each hosts
-    for host in orig_hosts:
-        message_data[host] = []
-        last_push[host] = dt.datetime.now()
     while True:
         logger.debug("Start waiting for new message in queue with queue size: {}".format(queue.qsize()))
         msg = queue.get()
         logger.info("Got new message. Queue size is now: {}".format(queue.qsize()))
-        # if queue.qsize() == 0 and hosts != orig_hosts:
-        #    hosts = list(orig_hosts)
-        #    logger.info("Resetting hosts %s", str(hosts))
         logger.debug("Data   : {}".format(msg.data))
         logger.debug("Subject: {}".format(msg.subject))
         logger.debug("Type   : {}".format(msg.type))
@@ -168,74 +143,17 @@ def read_from_queue(queue, logger, hosts, data_points_before_write):
         logger.debug("Version: {}".format(msg.version))
 
         if msg.type != "beat" and msg.type != 'ack':
-            # print "Version: " + str(mysql.connector.__version__)
             try:
                 msg_host = msg.host.split(".")[0]
             except Exception:
                 msg_host = msg.host
             message_data_point = (msg.subject, msg.time, msg_host, msg.type, json.dumps(msg.data, default=posttroll.message.datetime_encoder))
             logger.debug(message_data_point)
-            push_now = dt.datetime.now()
-            for host in hosts:
-                message_data[host].append(message_data_point)
-                if len(message_data[host]) > data_points_before_write or last_push[host] + dt.timedelta(seconds=60) < push_now:
-                    if host in skip_hosts:
-                        logger.info("Skipping host %s for now.", str(host))
-                        continue
-                    logger.debug("%s %s", host, str(message_data[host]))
-                    logger.info("Need to push data to db %s ...", str(host))
-                    try:
-                        cnx = mysql.connector.connect(user='polarsat', password='lilla land',
-                                                      host=host,
-                                                      database='pytrollmessages',
-                                                      connection_timeout=10)
 
-                    except mysql.connector.Error as err:
-                        skip_hosts.append(host)
-                        # After 10 minutes reset the skip_hosts list
-                        t = threading.Timer(10 * 60, reset_skip_hosts, args=(skip_hosts, ))
-                        t.start()
 
-                        # hosts.remove(host)
-                        # logger.info("Hosts is now: %s after removing %s", str(hosts), host)
-                        logger.info("Skip hosts is now: %s", str(skip_hosts))
-                        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                            logger.error("Something is wrong with your user name or password")
-                        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                            logger.error("Database does not exist")
-                        else:
-                            logger.error(err)
-                    else:
-                        message_insert = cnx.cursor(dictionary=True)
-                        try:
-                            # statement = "insert into messages (topic, datetime, msg_host, type, jdoc) value(\"{}\",\"{}\",\"{}\",\"{}\",'{}')".format(msg.subject, msg.time, msg.host, msg.type, json.dumps(msg.data, default=posttroll.message.datetime_encoder))
-                            message_insert.executemany(statement, message_data[host])
-                            cnx.commit()
-                            logger.info("Inserted %d into host %s", message_insert.rowcount, host)
-                            message_data[host] = []
-                        except mysql.connector.Error as err:
-                            logger.error("Failed insert message: {}".format(err))
-                        finally:
-                            message_insert.close()
-                            cnx.close()
-                    last_push[host] = dt.datetime.now()
-                else:
-                    logger.info("Wait for more messages before writing to db. %s Got %d of %d.", str(host), len(message_data[host]), data_points_before_write)
-
-        # logger.debug("{}".format())
-
-        # print "READ FROM QUEUE:",msg
-
-def write_to_queue(msg, meta, queue):
+def write_to_queue(msg, queue):
     # Write to queue
-    # print "WRITE TO QUEUE"
-    # print "Before write",queue.qsize()
-    # msg.data['db_database'] = meta['db_database']
-    # msg.data['db_passwd'] = meta['db_passwd']
-    # msg.data['db_user'] = meta['db_user']
-    # msg.data['db_host'] = meta['db_host']
     queue.put(msg)
-    # print "After write",queue.qsize()
 
 
 def arg_parse():
@@ -293,18 +211,9 @@ def main():
     logging.getLogger("posttroll").setLevel(logging.INFO)
     logger = logging.getLogger("MessageHandler")
 
-    try:
-        logger.info("config item: %s", args.config_item)
-        logger.info("db_hosts %s ", config.get(args.config_item, 'db_hosts'))
-        db_hosts = config.get(args.config_item, 'db_hosts').split(",")
-    except Exception:
-        logger.error("Failed to read db_hosts from config. use default")
-        db_hosts = ['157.249.169.223']
     queue = Queue()
 
-    data_points_before_write = config.getint(args.config_item, 'data_points_before_write')
-
-    queue_handler = Process(target=read_from_queue, args=(queue, logger, db_hosts, data_points_before_write,))
+    queue_handler = Process(target=read_from_queue, args=(queue, logger))
     queue_handler.daemon = True
     queue_handler.start()
     logger.info("queue handler pid %s", str(queue_handler.pid))
